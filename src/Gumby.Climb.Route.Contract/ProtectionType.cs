58adfeb08c493489478e291d57635817f709a4ca
Jim Hill <jim.hill.r@gmail.com>
2019-06-30 01:16:30 -0500

    ProtectionType display name

--- src/Gumby.Climb.Route.Contract/ProtectionType.cs
+++ src/Gumby.Climb.Route.Contract/ProtectionType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Gumby.Climb.Route.Contract
@@ -15,5 +16,40 @@ namespace Gumby.Climb.Route.Contract
         TRADLEAD,
         ROPESOLO,
         FREESOLO
+    }
+
+    public static class ProtectionTypeHelper
+    {
+        public static IList<ProtectionType> GetAll()
+        {
+            return Enum.GetValues(typeof(ProtectionType)).Cast<ProtectionType>().Where(pt => !pt.Equals(ProtectionType.NONE)).ToList();
+        }
+
+        public static string GetDisplayName(this ProtectionType protectionType)
+        {
+            switch (protectionType)
+            {
+                case ProtectionType.NONE:
+                    return "Missing";
+                case ProtectionType.BOULDER:
+                    return "Bouldering";
+                case ProtectionType.TOPROPE:
+                    return "Top Rope";
+                case ProtectionType.SPORTFOLLOW:
+                    return "Follow (Sport)";
+                case ProtectionType.TRADFOLLOW:
+                    return "Follow (Trad)";
+                case ProtectionType.SPORTLEAD:
+                    return "Lead (Sport)";
+                case ProtectionType.TRADLEAD:
+                    return "Lead (Trad)";
+                case ProtectionType.ROPESOLO:
+                    return "Solo (Rope)";
+                case ProtectionType.FREESOLO:
+                    return "Free Solo";
+                default:
+                    return "Missing";
+            }
+        }
     }
 }
