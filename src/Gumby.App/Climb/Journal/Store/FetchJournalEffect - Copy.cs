c1b40f62963c5ed63ba1abb1bb87708f125b66fa
Jim Hill <jim.hill.r@gmail.com>
2019-07-01 18:21:41 -0500

    Moved initial fetch to action

---
+++ src/Gumby.App/Climb/Journal/Store/FetchJournalEffect - Copy.cs
@@ -0,0 +1,19 @@
+﻿using Blazor.Fluxor;
+using Gumby.Climb.Journal.Contract;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gumby.App.Climb.Journal.Store
+{
+    public class JournalStoreInitializedEffect : Effect<StoreInitializedAction>
+    {
+        protected async override Task HandleAsync(StoreInitializedAction action, IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch(new FetchJournalAction());
+        }
+    }
+}
